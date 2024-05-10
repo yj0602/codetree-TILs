d172a19eda1457ba37c60e6aae3cb072977ec7d7
yj0602 <yj0602@pusan.ac.kr>
2024-05-10 19:23:39 +0900

    Update replacing-variable-values-3.py

--- 240510/변수 값 교체하기 3/replacing-variable-values-3.py
+++ 240510/변수 값 교체하기 3/replacing-variable-values-3.py
@@ -1,14 +1,7 @@
 a = 3
 b = 5
-
-# Store the value of 'a' in a temporary variable
 temp = a
-
-# Assign the value of 'b' to 'a'
 a = b
-
-# Assign the value stored in the temporary variable to 'b'
 b = temp
-
-print(a)  # This will print the value of 'b'
-print(b)  # This will print the value of 'a'
+print(a)
+print(b)  
