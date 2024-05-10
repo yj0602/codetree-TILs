89d16103eb2aafad6d4c3ea3c1bf84c519572710
yj0602 <yj0602@pusan.ac.kr>
2024-05-10 19:22:44 +0900

    Title: 변수 값 교체하기 3, Time: 100ms, Memory: 24MB, Status: Passed - CodeTree

--- 240510/변수 값 교체하기 3/replacing-variable-values-3.py
+++ 240510/변수 값 교체하기 3/replacing-variable-values-3.py
@@ -1,7 +1,14 @@
 a = 3
 b = 5
-a = temp
+
+# Store the value of 'a' in a temporary variable
+temp = a
+
+# Assign the value of 'b' to 'a'
 a = b
-temp = b
-print(a)
-print(b)
+
+# Assign the value stored in the temporary variable to 'b'
+b = temp
+
+print(a)  # This will print the value of 'b'
+print(b)  # This will print the value of 'a'
