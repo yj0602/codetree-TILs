1e0626d0cfe92ce7718a202ac8d8ac71bdc5e160
yj0602 <yj0602@pusan.ac.kr>
2024-05-14 21:44:05 +0900

    Title: 키를 기준으로 정렬, Time: 110ms, Memory: 24MB, Status: Passed - CodeTree

--- 240514/키를 기준으로 정렬/sort-by-height.py
+++ 240514/키를 기준으로 정렬/sort-by-height.py
@@ -1,18 +1,15 @@
-n = int(input())
-
 class Person:
     def __init__(self, name, height, weight):
         self.name = name
         self.height = height
         self.weight = weight
 
-people = [
-    Person("lee", 167, 40),
-    Person("kim", 149, 32),
-    Person("park", 161, 53),
-    Person("choi", 183, 70),
-    Person("jung", 155, 45)
-]
+n = int(input()) 
+people = []
+
+for _ in range(n):
+    name, height, weight = input().split()
+    people.append(Person(name, int(height), int(weight)))
 
 people.sort(key=lambda person: person.height)
 
