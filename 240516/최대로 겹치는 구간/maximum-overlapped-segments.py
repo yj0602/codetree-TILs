35709218703c2b4cc90e49db95dcc65dd46b895c
yj0602 <yj0602@pusan.ac.kr>
2024-05-16 21:29:57 +0900

    Title: 최대로 겹치는 구간, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240516/최대로 겹치는 구간/maximum-overlapped-segments.py
+++ 240516/최대로 겹치는 구간/maximum-overlapped-segments.py
@@ -3,6 +3,7 @@ A = [0] * 200
 
 for _ in range(N):
     l, r = map(int, input().split())
-    for i in range(1,r):
-        A(l+OFFSET) += 1
+    for i in range(l, r):  # 구간의 시작부터 끝까지 반복합니다.
+        A[l] += 1          # 해당 구간의 빈도수를 1 증가시킵니다.
+
 print(max(A))
