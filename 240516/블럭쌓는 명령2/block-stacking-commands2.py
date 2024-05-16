f33532cbf8c2bab34a1c091f12dca7601337659d
yj0602 <yj0602@pusan.ac.kr>
2024-05-16 21:05:24 +0900

    Title: 블럭쌓는 명령2, Time: 114ms, Memory: 25MB, Status: Passed - CodeTree

--- 240516/블럭쌓는 명령2/block-stacking-commands2.py
+++ 240516/블럭쌓는 명령2/block-stacking-commands2.py
@@ -1,1 +1,14 @@
-N, K = input().split()a
+n, k = tuple(map(int, input().split()))
+segments = [
+    tuple(map(int, input().split()))
+    for _ in range(k)
+]
+
+blocks = [0] * (n + 1)
+
+for a, b in segments:
+    for i in range(a, b + 1):
+        blocks[i] += 1
+
+max_num = max(blocks)
+print(max_num)
