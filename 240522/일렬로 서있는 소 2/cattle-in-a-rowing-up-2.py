94b7c7806bea9178ced88304a7fffa14c1458fdb
yj0602 <yj0602@pusan.ac.kr>
2024-05-22 20:36:52 +0900

    Title: 일렬로 서있는 소 2, Time: 127ms, Memory: 25MB, Status: Passed - CodeTree

--- 240522/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
+++ 240522/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
@@ -1,10 +1,12 @@
-N = int(input())
+# 변수 선언 및 입력
+n = int(input())
 arr = list(map(int, input().split()))
 
+# 모든 쌍을 다 잡아봅니다.
 cnt = 0
 for i in range(n):
-    for j in range(i+1, n):
-        for k in range(j+1, n):
+    for j in range(i + 1, n):
+        for k in range(j + 1, n):
             if arr[i] <= arr[j] and arr[j] <= arr[k]:
                 cnt += 1
 
