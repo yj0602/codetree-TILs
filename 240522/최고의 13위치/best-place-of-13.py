263e0197b721ed8068780ee37f6382539a3ac09e
yj0602 <yj0602@pusan.ac.kr>
2024-05-22 21:02:33 +0900

    Title: 최고의 13위치, Time: 131ms, Memory: 24MB, Status: Passed - CodeTree

--- 240522/최고의 13위치/best-place-of-13.py
+++ 240522/최고의 13위치/best-place-of-13.py
@@ -1,13 +1,16 @@
+# 변수 선언 및 입력
 n = int(input())
-
 arr = [
     list(map(int, input().split()))
     for _ in range(n)
 ]
 
+# 모든 쌍을 다 잡아봅니다.
 max_cnt = 0
 for i in range(n):
-    for j in range(n-2):
-        max_cnt = max(max_cnt, arr[i][j] + arr[j][j+2])
-
+    # 격자를 벗어나지 않을 범위로만 잡습니다.
+    for j in range(n - 2):
+        cnt = arr[i][j] + arr[i][j + 1] + arr[i][j + 2]
+        max_cnt = max(max_cnt, cnt)
+    
 print(max_cnt)
